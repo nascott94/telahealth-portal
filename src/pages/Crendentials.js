af558e21332406bbf323aed05387b4ffd360cd03
Dan Possehl <danielbpossehl@gmail.com>
2022-05-02 10:53:22 -0700

    all start pages finished for main pages

--- src/pages/Crendentials.js
+++ src/pages/Crendentials.js
@@ -1,7 +1,49 @@
 import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/homepage/Navbar";
+import Sidebar from "../components/homepage/Sidebar";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: row;
+`;
+
+const LeftContainer = styled.div`
+  height: 100vh;
+  width: 20%;
+  background-color: blue;
+`;
+
+const RightContainer = styled.div`
+  height: 100vh;
+  width: 80%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+`;
+
+const RightMain = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: blue;
+`;
 
 const Crendentials = () => {
-  return <div>Crendentials</div>;
+  return (
+    <>
+      <Container>
+        <LeftContainer>
+          <Sidebar />
+        </LeftContainer>
+        <RightContainer>
+          <Navbar></Navbar>
+          <RightMain></RightMain>
+        </RightContainer>
+      </Container>
+    </>
+  );
 };
 
 export default Crendentials;
