a4dcce617dd310f9d1d33553d7132e02b878841a
Dan Possehl <danielbpossehl@gmail.com>
2022-05-16 10:38:44 -0700

    account settings finished

--- src/components/profilepage/AccountSettings.js
+++ src/components/profilepage/AccountSettings.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "@emotion/styled";
 
 const Container = styled.div`
-  height: 100%;
+  height: 500px;
   width: 100%;
   background-color: orange;
   overflow: hidden;
@@ -135,7 +135,7 @@ const CountryRight = styled.div`
 
 const DropDownBox = styled.div`
   width: 90%;
-  height: 30%;
+  height: 30px;
   border: 1px solid black;
   display: flex;
   align-items: center;
@@ -164,8 +164,8 @@ const PassportRight = styled.div`
   justify-content: center;
 `;
 
-const RequireMFAContainer = styled.div`
-  height: 80px;
+const CheckBoxContainer = styled.div`
+  height: 100px;
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -203,6 +203,215 @@ const MFAContainerRight = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+`;
+
+const DateOfBirthContainer = styled.div`
+  height: 80px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid black;
+`;
+
+const DateOfBirthContainerTop = styled.div`
+  height: 30px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const DateOfBirthContainerBottom = styled.div`
+  height: 50px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const DateOfBirthContainerBottomLeft = styled.div`
+  height: 60px;
+  width: 33.3%;
+  display: flex;
+  align-items: center;
+`;
+
+const DateOfBirthContainerBottomMiddle = styled.div`
+  height: 60px;
+  width: 33.3%;
+  display: flex;
+  align-items: center;
+`;
+
+const DateOfBirthContainerBottomRight = styled.div`
+  height: 60px;
+  width: 33.3%;
+  display: flex;
+  align-items: center;
+`;
+
+const GenderPhotoIDContainer = styled.div`
+  height: 200px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid black;
+`;
+
+const GenderPhotoIDContainerLeft = styled.div`
+  height: 140px;
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const GenderPhotoIDContainerLeftTop = styled.div`
+  height: 80px;
+  width: 40%;
+  display: flex;
+  align-items: center;
+`;
+
+const GenderPhotoIDContainerLeftBottom = styled.div`
+  height: 100px;
+  width: 40%;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const GenderBubble = styled.div`
+  width: 30px;
+  height: 30px;
+  display: flex;
+`;
+
+const GenderPhotoIDContainerRight = styled.div`
+  height: 200px;
+  width: 50%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const UploadHeadshotContainer = styled.div`
+  height: 150px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid black;
+`;
+
+const UploadHeadshotContainerTop = styled.div`
+  height: 30px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const UploadHeadshotContainerBottom = styled.div`
+  height: 50px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const UploadHeadshotContainerBottomLeft = styled.div`
+  height: 90px;
+  width: 50%;
+  display: flex;
+  align-items: center;
+`;
+
+const UploadHeadshotContainerBottomMiddle = styled.div`
+  height: 90px;
+  width: 25%;
+  display: flex;
+  align-items: center;
+`;
+
+const UploadHeadshotContainerBottomRight = styled.div`
+  height: 90px;
+  width: 25%;
+  display: flex;
+  align-items: center;
+`;
+
+const EmergencyContactContainer = styled.div`
+  height: 400px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  border-bottom: 1px solid black;
+`;
+
+const EmergencyContactContainerTop = styled.div`
+  height: 50px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const EmergencyContactContainerBottom = styled.div`
+  height: 170px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const EmergencyContactBottomLeft = styled.div`
+  height: 150px;
+  width: 33.3%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const EmergencyContactBottomMiddle = styled.div`
+  height: 150px;
+  width: 33.3%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const EmergencyContactBottomRight = styled.div`
+  height: 150px;
+  width: 33.3%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const EmergancyContactBox = styled.div`
+  height: 50%;
+  width: 90%;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid black;
+`;
+
+const UpdateButtonContainer = styled.div`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const ButtonContainer = styled.div`
+  height: 70px;
+  width: 98%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+const UpdateButton = styled.div`
+  height: 40px;
+  width: 80px;
+  background-color: blue;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const AccountSettings = () => {
@@ -281,9 +490,9 @@ const AccountSettings = () => {
             <DropDownBox>Please Select</DropDownBox>
           </PassportRight>
         </PassportContainer>
-        <RequireMFAContainer>
+        <CheckBoxContainer>
           <h4>Require MFA for Login</h4>
-        </RequireMFAContainer>
+        </CheckBoxContainer>
         <MFAContainer>
           <MFAContainerLeft>
             <h4>MFA Alternate Code:</h4>
@@ -293,6 +502,96 @@ const AccountSettings = () => {
             <h5>Set-Up Multi Factor Authentification</h5>
           </MFAContainerRight>
         </MFAContainer>
+        <DateOfBirthContainer>
+          <DateOfBirthContainerTop>
+            <h4>Date of Birth</h4>
+          </DateOfBirthContainerTop>
+          <DateOfBirthContainerBottom>
+            <DateOfBirthContainerBottomLeft>
+              <DropDownBox></DropDownBox>
+            </DateOfBirthContainerBottomLeft>
+            <DateOfBirthContainerBottomMiddle>
+              <DropDownBox></DropDownBox>
+            </DateOfBirthContainerBottomMiddle>
+            <DateOfBirthContainerBottomRight>
+              <DropDownBox></DropDownBox>
+            </DateOfBirthContainerBottomRight>
+          </DateOfBirthContainerBottom>
+        </DateOfBirthContainer>
+        <GenderPhotoIDContainer>
+          <GenderPhotoIDContainerLeft>
+            <GenderPhotoIDContainerLeftTop>
+              <h4>Gender</h4>
+            </GenderPhotoIDContainerLeftTop>
+            <GenderPhotoIDContainerLeftBottom>
+              <GenderBubble>
+                <input type="radio" name="food" /> : Italian
+                <br />
+              </GenderBubble>
+              <GenderBubble>
+                <input type="radio" name="food" /> : Italian
+                <br />
+              </GenderBubble>
+              <GenderBubble>
+                <input type="radio" name="food" /> : Italian
+                <br />
+              </GenderBubble>
+            </GenderPhotoIDContainerLeftBottom>
+          </GenderPhotoIDContainerLeft>
+          <GenderPhotoIDContainerRight>
+            <h4>Photo ID</h4>
+            <h5>
+              Attach a photo of the patient's (if over 18) or guardian's photo
+              ID.
+            </h5>
+            <h2>Choose File</h2>
+            <h4>Capture Using Webcam</h4>
+            <h5>
+              Only .jpeg, .jpg, .png, .gif files are allowed. File size should
+              not be greater than 5MB.
+            </h5>
+          </GenderPhotoIDContainerRight>
+        </GenderPhotoIDContainer>
+        <UploadHeadshotContainer>
+          <UploadHeadshotContainerTop>
+            <h4>Upload Headshot</h4>
+          </UploadHeadshotContainerTop>
+          <UploadHeadshotContainerBottom>
+            <UploadHeadshotContainerBottomLeft>
+              <DropDownBox>Choose file</DropDownBox>
+            </UploadHeadshotContainerBottomLeft>
+            <UploadHeadshotContainerBottomMiddle>
+              <DropDownBox>Choose file</DropDownBox>
+            </UploadHeadshotContainerBottomMiddle>
+            <UploadHeadshotContainerBottomRight>
+              <DropDownBox>Choose file</DropDownBox>
+            </UploadHeadshotContainerBottomRight>
+          </UploadHeadshotContainerBottom>
+          <h5>File size should not be greater than 5MB.</h5>
+        </UploadHeadshotContainer>
+        <EmergencyContactContainer>
+          <EmergencyContactContainerTop>
+            <h4>Emergency Contact Information :</h4>
+          </EmergencyContactContainerTop>
+          <EmergencyContactContainerBottom>
+            <EmergencyContactBottomLeft>
+              <EmergancyContactBox>First Name</EmergancyContactBox>
+              <EmergancyContactBox>Home Phone</EmergancyContactBox>
+            </EmergencyContactBottomLeft>
+            <EmergencyContactBottomMiddle>
+              <EmergancyContactBox>Last Name</EmergancyContactBox>
+              <EmergancyContactBox>Cell Phone</EmergancyContactBox>
+            </EmergencyContactBottomMiddle>
+            <EmergencyContactBottomRight>
+              <EmergancyContactBox>Email</EmergancyContactBox>
+            </EmergencyContactBottomRight>
+          </EmergencyContactContainerBottom>
+        </EmergencyContactContainer>
+        <UpdateButtonContainer>
+          <ButtonContainer>
+            <UpdateButton>Update</UpdateButton>
+          </ButtonContainer>
+        </UpdateButtonContainer>
       </Container>
     </>
   );
