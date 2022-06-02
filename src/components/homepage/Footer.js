2d790ab17432e6326ebe5bf477f01c195dffe613
Dan Possehl <danielbpossehl@gmail.com>
2022-06-01 18:44:06 -0700

    footer

--- src/components/homepage/Footer.js
+++ src/components/homepage/Footer.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import CopyrightIcon from '@mui/icons-material/Copyright';
+import React from "react";
+import styled from "styled-components";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+
+const FooterContainer = styled.div`
+  height: 10%;
+  width: 100%;
+  display: flex;
+`;
+
+const FooterLeft = styled.div`
+  height: 100%;
+  width: 30%;
+  background-color: purple.;
+`;
+
+const FooterRight = styled.div`
+  height: 100%;
+  width: 30%;
+`;
 
 const Footer = () => {
   return (
     <div>
-      <CopyrightIcon />
-      Footer
+      <FooterContainer>
+        <FooterLeft></FooterLeft>
+        <FooterRight></FooterRight>
+      </FooterContainer>
     </div>
   );
 };
